dc005ff1f828e55508a7285d7bb312399d1982c2
Misan Parajuli <150371766+MisanParajuli@users.noreply.github.com>
2024-08-01 21:39:12 -0500

    Add files via upload

---
+++ untitled12/main.cpp
@@ -0,0 +1,21 @@
+//User provides 10 random numbers, the program will choose the largest
+// and the smallest number and find average
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int SIZE;
+
+    cout << "Enter the number of vectors ";
+    cin >> SIZE;
+
+    vector<int> num;
+
+
+
+
+    return 0;
+}
